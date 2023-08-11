730f50486a6b48cc6ada0dc6461361ee510e323c
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-08-11 21:41:49 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
 import { Button } from '@/components/ui/button'
 import { Inter } from 'next/font/google'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,10 +17,21 @@ export default function Home() {
       className={`max-w-5xl m-auto grid grid-cols-1 md:grid-cols-2 px-3  ${inter.className}`}
     >
       <div className='relative'>
-        <div className='block md:fixed top-1/2'>
-          <Button>
-            Button
-          </Button>
+        <div className='w-full md:w-80 block md:fixed top-1/2 md:-translate-y-1/2'>
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Button>
+                Button
+              </Button>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
         </div>
       </div>
       <div>
