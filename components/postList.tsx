1e4b5f5b328cf371070828ba2bf29d4ab3d5250f
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-08-13 21:44:43 +0200

    added tags

--- components/postList.tsx
+++ components/postList.tsx
@@ -6,57 +6,65 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+import { Badge } from "./ui/badge";
 
 const posts = [
-    {
-      id: 1,
-      title: "Exploring the Wonders of Nature",
-      date: "2023-08-01",
-      description: "A captivating journey through lush forests, majestic mountains, and serene lakes.",
-      image: "https://via.placeholder.com/300"
-    },
-    {
-      id: 2,
-      title: "Unveiling the Secrets of Ancient Civilizations",
-      date: "2023-07-15",
-      description: "Delve into the mysteries of long-lost cultures and their remarkable achievements.",
-      image: "https://via.placeholder.com/300"
-    },
-    {
-      id: 3,
-      title: "Mastering the Art of Culinary Delights",
-      date: "2023-06-20",
-      description: "Embark on a culinary adventure, exploring flavors from around the world and learning expert techniques.",
-      image: "https://via.placeholder.com/300"
-    },
-    {
-      id: 4,
-      title: "Astronomy: Discovering the Cosmos",
-      date: "2023-05-10",
-      description: "Peer through telescopes to unravel the mysteries of stars, galaxies, and beyond.",
-      image: "https://via.placeholder.com/300"
-    },
-    {
-      id: 5,
-      title: "The Evolution of Technology",
-      date: "2023-04-02",
-      description: "Trace the history of innovation, from the first tools to cutting-edge advancements.",
-      image: "https://via.placeholder.com/300"
-    },
-    {
-      id: 6,
-      title: "Exploring Remote Wilderness",
-      date: "2023-03-15",
-      description: "Embark on an expedition to untouched landscapes, discovering hidden treasures of nature.",
-      image: "https://via.placeholder.com/300"
-    },
-    {
-      id: 7,
-      title: "Art and Culture: Bridging the Past and Present",
-      date: "2023-02-08",
-      description: "Immerse yourself in the rich tapestry of human creativity, from ancient artworks to modern expressions.",
-      image: "https://via.placeholder.com/300"
-    }
+  {
+    id: 1,
+    title: "Exploring the Wonders of Nature",
+    date: "2023-08-01",
+    description: "A captivating journey through lush forests, majestic mountains, and serene lakes.",
+    image: "https://via.placeholder.com/300",
+    tags: ["travel", "nature", "adventure"]
+  },
+  {
+    id: 2,
+    title: "Unveiling the Secrets of Ancient Civilizations",
+    date: "2023-07-15",
+    description: "Delve into the mysteries of long-lost cultures and their remarkable achievements.",
+    image: "https://via.placeholder.com/300",
+    tags: ["history", "archaeology"]
+  },
+  {
+    id: 3,
+    title: "Mastering the Art of Culinary Delights",
+    date: "2023-06-20",
+    description: "Embark on a culinary adventure, exploring flavors from around the world and learning expert techniques.",
+    image: "https://via.placeholder.com/300",
+    tags: ["food", "cooking", "travel"]
+  },
+  {
+    id: 4,
+    title: "Astronomy: Discovering the Cosmos",
+    date: "2023-05-10",
+    description: "Peer through telescopes to unravel the mysteries of stars, galaxies, and beyond.",
+    image: "https://via.placeholder.com/300",
+    tags: ["science", "astronomy", "space", "exploration"]
+  },
+  {
+    id: 5,
+    title: "The Evolution of Technology",
+    date: "2023-04-02",
+    description: "Trace the history of innovation, from the first tools to cutting-edge advancements.",
+    image: "https://via.placeholder.com/300",
+    tags: ["technology", "history", "innovation"]
+  },
+  {
+    id: 6,
+    title: "Exploring Remote Wilderness",
+    date: "2023-03-15",
+    description: "Embark on an expedition to untouched landscapes, discovering hidden treasures of nature.",
+    image: "https://via.placeholder.com/300",
+    tags: ["travel", "nature", "adventure", "exploration"]
+  },
+  {
+    id: 7,
+    title: "Art and Culture: Bridging the Past and Present",
+    date: "2023-02-08",
+    description: "Immerse yourself in the rich tapestry of human creativity, from ancient artworks to modern expressions.",
+    image: "https://via.placeholder.com/300",
+    tags: ["art", "culture", "history"]
+  }
 ];
   
 
@@ -81,7 +89,9 @@ export default function PostList() {
                         </p>
                     </CardContent>
                     <CardFooter>
-                        <p>Card Footer</p>
+                      <div className="flex flex-wrap gap-1">
+                        {item.tags.map((tag, index) => (<Badge key={index} variant="outline">{tag}</Badge>))}
+                      </div>
                     </CardFooter>
                 </Card>
             ))}
