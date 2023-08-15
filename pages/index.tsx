504eb249d00983d4f2625371c57152f82b7d7fac
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-08-15 13:20:19 +0200

    refactor, changed fixed position to sticky

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import { Inter } from 'next/font/google'
-
 import PostList from '@/components/postList'
 import ContentTabs from '@/components/contentTabs'
 
@@ -8,10 +7,10 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main
-      className={`max-w-5xl m-auto grid grid-cols-1 md:grid-cols-2 px-3  ${inter.className}`}
+      className={`max-w-5xl m-auto gap-x-20 gap-y-6 grid grid-cols-1 md:grid-cols-2 px-3  ${inter.className}`}
     >
-      <div className='relative'>
-        <div className='w-full md:w-80 block md:fixed top-1/2 md:-translate-y-1/2'>
+      <div>
+        <div className='md:sticky top-1/2 -translate-y-1/2'>
           <ContentTabs/>
         </div>
       </div>
