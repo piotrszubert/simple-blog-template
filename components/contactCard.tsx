7c88897f010b8a722579f5e36cf449a4f9d6e0ba
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-08-16 21:22:41 +0200

    Update contactCard.tsx

--- components/contactCard.tsx
+++ components/contactCard.tsx
@@ -1,4 +1,12 @@
 import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {
     Form,
     FormControl,
     FormDescription,
@@ -9,12 +17,14 @@ import {
 } from "@/components/ui/form"
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea"
 import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form";
 
 const formSchema = z.object({
     email: z.string().min(2).max(50),
+    message: z.string().min(2).max(250),
 })
 
 export default function ContactCard() {
@@ -23,6 +33,7 @@ export default function ContactCard() {
         resolver: zodResolver(formSchema),
         defaultValues: {
           email: "",
+          message: "",
         },
     })
 
@@ -31,26 +42,49 @@ export default function ContactCard() {
     }
 
     return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Email</FormLabel>
-                            <FormControl>
-                                <Input placeholder="shadcn" {...field} />
-                            </FormControl>
-                            <FormDescription>
-                                This is your public display name.
-                            </FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit">Submit</Button>
-            </form>
-        </Form>
+        <Card>
+        <CardHeader>
+            <CardTitle>Contact Us</CardTitle>
+            <CardDescription>Please use the form below to get in touch with us.</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <>
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input type="email" placeholder="email@example.com"  {...form.register("email")} />
+                                    </FormControl>
+                                    {/* <FormDescription>
+                                        Leave your email so we can reply you
+                                    </FormDescription> */}
+                                    <FormMessage />
+                                </FormItem>
+                                <FormItem>
+                                    <FormLabel>Message</FormLabel>
+                                    <FormControl>
+                                        <Textarea   {...form.register("message")} />
+                                    </FormControl>
+                                    {/* <FormDescription>
+                                        Leave your email so we can reply you
+                                    </FormDescription> */}
+                                    <FormMessage />
+                                </FormItem>
+                            </>
+                        )}
+                    />
+                    <Button type="submit">Submit</Button>
+                </form>
+            </Form>
+        </CardContent>
+        <CardFooter>
+            {/* <p>Card Footer</p> */}
+        </CardFooter>
+        </Card>
     );
 }
