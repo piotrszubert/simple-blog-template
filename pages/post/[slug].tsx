34af6a9f59b25efe57c46981566eedaeebd1553f
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-08-19 09:01:48 +0200

    read slug parameter with useRouter

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,12 +1,18 @@
 import { Inter } from "next/font/google";
+import { useRouter } from "next/router";
 const inter = Inter({ subsets: ['latin'] })
 
-export default function PostPage() {
-    return (
 
+export default function PostPage() {
+    const router = useRouter();
+
+    return (
         <main
             className={`max-w-5xl m-auto px-3 ${inter.className}`}
         >
+            <h1>
+                {router.query.slug}
+            </h1>
             <div>
                 Post page
             </div>
