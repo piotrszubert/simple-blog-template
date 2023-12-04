73a1d7d36498cc3c772ac6a6926e20811c7425d2
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-12-04 11:12:27 +0000

    tailwind typography plugin

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -16,19 +16,17 @@ const mdxComponents: MDXComponents = {
     <Image src={src} width={width} height={height} alt={alt} />
   ),
 
-  Alert: ({title, children}) => (
-    <div role="alert" className="alert border-2 shadow-lg border-info">
+  Alert: ({ title, children }) => (
+    <div role="alert" className="alert border-info border-2 shadow-lg">
       <div className="px-5">
         <Info width={32} height={32} className="text-info" />
       </div>
       <div>
-        <h3 className="font-bold mt-0 -mb-2">{title}</h3>
-        <div className="text-sm">
-          {children}
-        </div>
+        <h3 className="-mb-2 mt-0 font-bold">{title}</h3>
+        <div className="text-sm">{children}</div>
       </div>
     </div>
-  )
+  ),
 }
 
 export const generateStaticParams = async () =>
@@ -37,7 +35,7 @@ export const generateStaticParams = async () =>
 export const generateMetadata = ({ params }: { params: { slug: string } }) => {
   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug)
   if (!post) notFound()
-return { title: `${post.title}` }
+  return { title: `${post.title}` }
 }
 
 export default function PostPage({ params }: { params: { slug: string } }) {
@@ -48,28 +46,21 @@ export default function PostPage({ params }: { params: { slug: string } }) {
   const MDXContent = useMDXComponent(post.body.code)
 
   return (
-    <article className="py-8 max-w-xl prose prose-lg mx-auto">
-      <div className="mb-8 text-center">
-        <time dateTime={post.date} className="mb-1 text-sm">
-          {format(parseISO(post.date), "LLLL d, yyyy")}
-        </time>
-        <h1 className="text-5xl font-bold mb-12">{post.title}</h1>
-      </div>
-      <MDXContent components={mdxComponents} />
-      <div className="divider my-8"></div>
-      <div
-        className="text-center"
-      >
-        <Button
-          variant="outline"
-        asChild>
-          <Link
-            href="/"
-            >
-            See all posts
-          </Link>
+    <main>
+      <article className="prose prose-lg prose-stone mx-auto max-w-xl py-8 dark:prose-invert">
+        <div className="mb-8 text-center">
+          <time dateTime={post.date} className="mb-1 text-sm">
+            {format(parseISO(post.date), "LLLL d, yyyy")}
+          </time>
+          <h1 className="mb-12 text-5xl font-bold">{post.title}</h1>
+        </div>
+        <MDXContent components={mdxComponents} />
+      </article>
+      <div className="text-center">
+        <Button variant="outline" asChild>
+          <Link href="/">See all posts</Link>
         </Button>
       </div>
-    </article>
+    </main>
   )
 }
